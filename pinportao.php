7616878b29f21dc191137d376c6b250e6f25d53e
Naamã Lima <naama32@hotmail.com>
2020-03-05 22:19:37 -0300

    ajusta função para desligar pino

--- pinportao.php
+++ pinportao.php
@@ -3,7 +3,7 @@
         //define o pino como saída
        	system("gpio -g mode 2 out");
        	//define o pino no estado 0, ativa o relê
-        system("gpio -g write 2 0");
+        system("gpio -g write 2 0 && sleep .5 &&  gpio -g write 2 1");
        
        	$mensagem = "Portão acionado em ". date('d/m/Y')." às ". date('H:i'). "\n";
         
