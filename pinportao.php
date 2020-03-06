2de27ea37949ca3ea17e83b22ee82aa87de8831c
Naamã Lima <naama32@hotmail.com>
2020-03-05 23:11:23 -0300

    altera pino

--- pinportao.php
+++ pinportao.php
@@ -1,9 +1,9 @@
 <?php	
     if($_POST) {
         //define o pino como saída
-       	system("gpio -g mode 2 out");
+       	system("gpio -g mode 9 out");
        	//define o pino no estado 0, ativa o relê
-        system("gpio -g write 2 0 && sleep .5 &&  gpio -g write 2 1");
+        system("gpio -g write 9 0 && sleep .5 &&  gpio -g write 9 1");
        
        	$mensagem = "Portão acionado em ". date('d/m/Y')." às ". date('H:i'). "\n";
         
